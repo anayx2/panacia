4ae1ecd21e57522ca6bff49d86abdbd8db39ef3a
anayx2 <anay@alphabettechnology.in>
2025-01-10 19:06:59 +0530

    service details page added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -28,11 +28,29 @@ export default {
     					opacity: '1',
     					transform: 'translateY(0)'
     				}
+    			},
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
     			}
     		},
     		animation: {
     			'fade-down': 'fade-down 0.8s ease-out',
-    			'fade-up': 'fade-up 0.8s ease-out'
+    			'fade-up': 'fade-up 0.8s ease-out',
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
     		},
     		fontFamily: {
     			poppins: [
